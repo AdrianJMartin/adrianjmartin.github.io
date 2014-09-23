e2068af1aeade8da2436e8d2593e6ec9fe3bf2cf
adrianjmartin <AdrianJMartin@Gmail.com>
2014-09-23 22:58:06 +0100

    mcd data binging

--- app.js
+++ app.js
@@ -33,5 +33,27 @@ controllers.controller( "twoCtrl" ,
 controllers.controller( "mceCtrl" ,
   function($scope){
 
+    $scope.data = {text:"abc"};
+
+    var mce_script = document.querySelector("script#mce");
+
+    var mceInit = function(){
+      tinyMCE.init({selector:"textarea"});
+      tinyMCE.get("mce_textarea").on("change",function(e){
+        console.log(arguments);
+        $scope.data.text = e.target.getContent();
+      });
+    }
+
+    if( mce_script ){
+      mceInit();
+    }else{
+       var s = document.createElement('script'); // use global document since Angular's $document is weak
+       s.id = "mce_script";
+       s.src = '//tinymce.cachefly.net/4.0/tinymce.min.js';
+       s.addEventListener("load", mceInit );
+       document.head.appendChild(s);
+    }
+
   }
 );
